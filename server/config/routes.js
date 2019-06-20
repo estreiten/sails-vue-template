39f772c5f446aa169d47483b5cbb603da7b98bf2
asadito <streitenberger.ezequiel@gmail.com>
2019-06-20 20:46:37 -0300

    fix: server routes indentation

--- server/config/routes.js
+++ server/config/routes.js
@@ -21,6 +21,6 @@ module.exports.routes = {
   * not match any of those, it is matched against static assets.             *
   *                                                                          *
   ***************************************************************************/
- 'GET /api/test': 'TestController.dummy'
+  'GET /api/test': 'TestController.dummy'
 
 };
